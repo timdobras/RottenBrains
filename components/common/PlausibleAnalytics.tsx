629de760ecf373b27d89b6f592c7d2efe8aed5ec
Gumeq <GumekTV@gmail.com>
2026-01-06 01:31:30 +0200

    feat: Replace analytics with self-hosted Plausible
    - Removed Vercel Analytics and Google Analytics components.
    - Added a new PlausibleAnalytics component to handle Plausible integration.
    - The Plausible component is added to the main layout, commented out, for easy configuration.
    - The old GoogleAnalytics.tsx file has been deleted.

---
+++ components/common/PlausibleAnalytics.tsx
@@ -0,0 +1,24 @@
+'use client';
+
+import React from 'react';
+
+interface Props {
+  domain: string;
+  src?: string;
+}
+
+const PlausibleAnalytics: React.FC<Props> = ({ domain, src = 'https://plausible.io/js/script.js' }) => {
+  return (
+    <>
+      <script defer data-domain={domain} src={src}></script>
+      <script>
+        {`
+          window.plausible=window.plausible||function(){(plausible.q=plausible.q||[]).push(arguments)},plausible.init=plausible.init||function(i){plausible.o=i||{}};
+          plausible.init();
+        `}
+      </script>
+    </>
+  );
+};
+
+export default PlausibleAnalytics;
